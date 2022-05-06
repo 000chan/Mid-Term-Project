b594e9fb3cab71a49fa16232d3238660e32bd708
000chan <32170786@dankook.ac.kr>
2022-05-06 16:47:06 +0900

    commit5

--- theme/urls.py
+++ theme/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import *
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', MainpageView.as_view(), name='mainpage'),
     path('aboutChan/', AboutpageView.as_view(), name='aboutpage'),
+    path('aboutChan/aboutChan', RedirectView.as_view(url="/home/aboutChan/")),
+    path('aboutChan/contactChan', RedirectView.as_view(url="/home/contactChan/")),
     path('contactChan/', ContactpageView.as_view(), name='contactpage'),
+    path('contactChan/', RedirectView.as_view(url="/contactChan/")),
+    path('contactChan/contactChan', RedirectView.as_view(url="/home/contactChan/")),
+    path('contactChan/aboutChan', RedirectView.as_view(url="/home/aboutChan/")),
 ]
