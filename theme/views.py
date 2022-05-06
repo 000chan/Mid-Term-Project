cf8bd0c3a13e364f9f637d15557bb261314b9987
000chan <32170786@dankook.ac.kr>
2022-05-06 14:13:29 +0900

    commit4

--- theme/views.py
+++ theme/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from re import template
 from django.views.generic.base import TemplateView
 
@@ -10,9 +9,3 @@ class AboutpageView(TemplateView):
 
 class ContactpageView(TemplateView):
     template_name = 'theme/main_contact.html'
-=======
-from django.views.generic.base import TemplateView
-
-class MainpageView(TemplateView):
-    template_name = 'theme/main.html'
->>>>>>> 4dceddee5016159518f7fcf4efe6afecd4ad1d72
